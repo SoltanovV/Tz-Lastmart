0bcd8fff074f033acc04c5df5ca472a26ae15815
VladislavSoltanov <vsoltanovv@gmail.com>
2022-08-06 17:51:49 +0200

    Подключение сервисов

--- Tz-Lastmart/Program.cs
+++ Tz-Lastmart/Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Tz_Lastmart.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
