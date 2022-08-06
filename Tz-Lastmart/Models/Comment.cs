a6a021a672c66a177441c1858676a7cbd5928865
VladislavSoltanov <vsoltanovv@gmail.com>
2022-08-06 17:50:39 +0200

    Создание моделей

---
+++ Tz-Lastmart/Models/Comment.cs
@@ -0,0 +1,27 @@
+﻿namespace Tz_Lastmart.Models
+{
+    public class Comment
+    {
+        /// <summary>
+        /// Id комментария
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Текст комментария
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Цвет блока
+        /// </summary>
+        public string BackgroundColor { get; set; }
+
+        /// <summary>
+        /// Ключ 
+        /// </summary>
+        public int PointId { get;set; }
+
+        public Point Point { get; set; }
+    }
+}
---
+++ Tz-Lastmart/Models/Point.cs
@@ -0,0 +1,36 @@
+﻿namespace Tz_Lastmart.Models;
+
+public class Point
+{
+    /// <summary>
+    /// Id точки
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Координаты по X
+    /// </summary>
+    public int X { get; set; }
+
+    /// <summary>
+    /// Координаты по Y
+    /// </summary>
+    public int Y { get; set; }
+
+    /// <summary>
+    /// Радиус точки
+    /// </summary>
+    public int Radius { get; set; }
+
+    /// <summary>
+    /// Цвет точки
+    /// </summary>
+    public string Color { get; set; }
+
+    /// <summary>
+    /// Ключ
+    /// </summary>
+    public int CommentId { get; set; }
+
+    public IEnumerable<Comment> Comment{ get; set; }
+}
